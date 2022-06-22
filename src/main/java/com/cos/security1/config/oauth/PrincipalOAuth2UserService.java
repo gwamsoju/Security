4f6103b89a60f8223f5b0e215af662fc1844f624
LEE Jaeyeong <dlwodud1207@naver.com>
2022-06-22 17:35:53 +0900

    facebook 로그인 추가

--- src/main/java/com/cos/security1/config/oauth/PrincipalOAuth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOAuth2UserService.java
@@ -1,6 +1,9 @@
 package com.cos.security1.config.oauth;
 
 import com.cos.security1.config.auth.PrincipalDetails;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
+import com.cos.security1.config.oauth.provider.facebookUserInfo;
 import com.cos.security1.repository.UserRepository;
 import com.cos.security1.vo.User1;
 import lombok.RequiredArgsConstructor;
@@ -30,11 +33,24 @@ public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
         // userRequest 정보 -> loadUser함수 호출 -> 구글로부터 회원프로필 받음
         System.out.println("getAttributes : " +oAuth2User.getAttributes());
 
-        String provider = userRequest.getClientRegistration().getClientId(); // google
-        String providerId = oAuth2User.getAttribute("sub");
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
+            System.out.println("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")){
+            System.out.println("페이스북 로그인 요청");
+            oAuth2UserInfo = new facebookUserInfo(oAuth2User.getAttributes());
+
+        }else{
+            System.out.println("페이스북과 구글만 지원한다.");
+        }
+
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
         String username = provider + "_" + providerId;
         String password = bCryptPasswordEncoder.encode("겟인데어");
-        String email = oAuth2User.getAttribute("email");
+        String email = oAuth2UserInfo.getEmail();
         String role = "ROLE_USER";
 
         User1 userEntity = userRepository.findByUsername(username);
